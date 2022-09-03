c5e816f9b25f24e85026ac6b6cb2e6a68b3abe8a
Author <author@example.com>
2022-09-03 20:43:44 +0300

    Remove redundant PiggyBank definition.

--- contracts/PiggyBankFactory.sol
+++ contracts/PiggyBankFactory.sol
@@ -5,6 +5,8 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import './PiggyBank.sol';
+
 contract PiggyBankFactory {
     mapping(address => address[]) private addressToPiggyBanks;
 
@@ -32,30 +34,6 @@ contract PiggyBankFactory {
     function getPiggyBanksByAddress(address _address) public view returns (address[] memory) {
         return addressToPiggyBanks[_address];
     }
-}
-
-abstract contract PiggyBank {
-    address public owner;
-    bool public isOver;
-    string public desc;
-
-    constructor(address _owner, string memory _desc) {
-        owner = _owner;
-        desc = _desc;
-    }
-
-    function deposit() public payable {
-        require(!isOver, "This piggy bank in over!");
-    }
-
-    function withdraw() public {
-        require(msg.sender == owner, "You are not an owner!");
-        require(isWithdrawAvailable(), "You can't do withdraw yet");
-        payable(owner).transfer(address(this).balance);
-        isOver = true;
-    }
-
-    function isWithdrawAvailable() public view virtual returns (bool) {}
 }
 
 contract TimePiggyBank is PiggyBank {
