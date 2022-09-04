dcdcd16a79bf170d93585512d9117ce50d9a33a0
Yuri.P <domosedov@mail.ru>
2022-09-04 22:22:21 +0300

    AlexP_vs_Suert_vs_Domosedov_04-09-2022_01

---
+++ components/Header copy.js
@@ -0,0 +1,52 @@
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import provider from "../contracts/metamaskProvider";
+
+const Header = () => {
+  const [currentAccount, setCurrentAccount] = useState();
+  // const address = currentAccount.toString();
+  console.log("Account", currentAccount);
+
+  useEffect(() => {
+    sessionStorage.getItem("currentAccount") &&
+      setCurrentAccount(sessionStorage.getItem("currentAccount"));
+  }, []);
+
+  const handleConnectMetamaskClick = async () => {
+    try {
+      const accounts = await provider.send("eth_requestAccounts", []);
+      sessionStorage.setItem("currentAccount", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <div className="mt-4 flex justify-between">
+      <div>
+        <Link href="/">
+          <a className="text-2xl">Home</a>
+        </Link>
+        <Link href="/piggy_banks">
+          <a className="ml-4 text-2xl">Piggy Banks</a>
+        </Link>
+      </div>
+      {!currentAccount ? (
+        <button
+          className="rounded-2xl border-2 border-orange-300 px-[15px] py-2 text-xl hover:bg-orange-300"
+          onClick={handleConnectMetamaskClick}
+        >
+          Connect Metamask
+        </button>
+      ) : (
+        <button className="hover:bg-o rounded-2xl border-2 border-orange-300 px-[15px] py-2 text-xl">
+          {currentAccount.toString().slice(0, 5) +
+            "..." +
+            currentAccount.toString().slice(38)}
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default Header;
