fca67c1527f7da8357e3cac4004116030a856b70
Suert <vaniaurdenko@gmail.com>
2022-09-16 04:15:02 +0200

    updated again what I have forgotten

--- client/pages/piggy_banks/new/index.js
+++ client/pages/piggy_banks/new/index.js
@@ -100,26 +100,26 @@ const NewPiggyBankPage = () => {
 
   return (
     <Layout>
-      <h1 className="text-4xl">Create you piggy bank!</h1>
-      <form className="my-4" onSubmit={handleCreateSubmit}>
-        <label className="text-2xl" htmlFor="owner">
+      <h1 className="text-4xl ml-36 mt-6 mb-6">Create your piggy bank!</h1>
+      <form className="max-w-6xl ml-6 bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 border-pink-300 border-2" onSubmit={handleCreateSubmit}>
+        <label className="block text-pink-500 text-3xl" htmlFor="owner">
           Enter owner address:
         </label>
         <br />
         <input
-          className="my-2 w-1/2 rounded border border-orange-300 py-1 px-4 text-xl "
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-xl leading-tight focus:outline-none focus:shadow-outline"
           ref={ownerRef}
           name="owner"
           type="text"
           placeholder="enter owner address"
         />
         <br />
-        <label className="text-2xl" htmlFor="desc">
+        <label className="block text-pink-500 text-3xl mt-3" htmlFor="desc">
           Enter description:
         </label>
         <br />
         <textarea
-          className="my-2 w-1/2 rounded border border-orange-300 py-1 px-4 text-xl "
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-xl leading-tight focus:outline-none focus:shadow-outline"
           ref={descRef}
           name="desc"
           type="text"
@@ -130,7 +130,7 @@ const NewPiggyBankPage = () => {
 
         {/* добавил Select для изменения типа создаваемой копилки */}
         <div>
-          <label htmlFor="countries" className="text-2xl">
+          <label htmlFor="countries" className="block text-pink-500 text-3xl">
             Select a type Piggy Bank
           </label>
           <select
@@ -154,12 +154,12 @@ const NewPiggyBankPage = () => {
 
         {piggyBankType === "amount" ? (
           <div>
-            <label className="text-2xl" htmlFor="amount">
+            <label className="block text-pink-500 text-3xl mt-3" htmlFor="amount">
               Enter amount for Piggy (eth):
             </label>
             <br />
             <input
-              className="my-2 w-1/2 rounded border border-orange-300 py-1 px-4 text-xl "
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-xl leading-tight focus:outline-none focus:shadow-outline "
               ref={amountRef}
               name="amount"
               type="text"
@@ -168,11 +168,11 @@ const NewPiggyBankPage = () => {
           </div>
         ) : (
           <div>
-            <label className="text-2xl" htmlFor="date">
+            <label className="block text-pink-500 text-3xl" htmlFor="date">
               Enter end date in uts:
             </label>
             <br />
-            <div className="mt-4 w-1/2 p-2 rounded border border-orange-300">
+            <div className="shadow appearance-none border rounded w-full font-bold text-gray-500 py-4 px-6 leading-tight focus:outline-none focus:shadow-outline">
               <DatePicker
                 selected={startDate}
                 onChange={(date) => setStartDate(date)}
@@ -184,13 +184,13 @@ const NewPiggyBankPage = () => {
 
         <br />
         {currentAccount ? 
-        <button disabled = {loader} className="my-2 w-1/4 rounded border border-orange-300 py-1 px-4 text-xl hover:bg-orange-300"type="submit">
+        <button disabled = {loader} className="rounded border bg-pink-100 border-pink-300 py-1 px-4 text-xl hover:bg-pink-300"type="submit">
           Create
         </button> :
         <div className="text-4xl text-center text-red-500 font-bold">Please connect with your wallet</div>}
       </form>
-      {loader ? <div role="status">
-          <svg aria-hidden="true" class="mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-pink-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+      {loader ? <div className="ml-16" role="status">
+          <svg aria-hidden="true" class="mr-2 w-16 h-16 text-gray-200 animate-spin dark:text-gray-600 fill-pink-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
             <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
           </svg>
